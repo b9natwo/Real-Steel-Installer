327dab118137146a130322513dfc423f04504b57
b9na <111341708+b9natwo@users.noreply.github.com>
2024-06-02 23:37:09 -0700

    Add files via upload

---
+++ run_xenia_canary.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "%~dp0XeniaCanary"
+start "" /MAX xenia_canary.exe "%~dp062939F79719792E4C7A3023F3C63E17428928B4D58"
---
+++ run_xenia_stfs.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "%~dp0XeniaSTFS"
+start "" /MAX xenia.exe "%~dp062939F79719792E4C7A3023F3C63E17428928B4D58"
